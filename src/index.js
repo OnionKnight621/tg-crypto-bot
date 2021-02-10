8badd8a28e96c15354a276f534f4e786d6a74598
onionknight621 <onionknight621@gmail.com>
2021-02-10 17:51:43 +0200

    upd: sheduled pinger

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const { Telegraf } = require('telegraf');
 const schedule = require('node-schedule');
 const mongoose = require('mongoose');
 const express = require('express');
+const request = require('request');
 const expressApp = express();
 
 const configs = require('./configs');
@@ -277,7 +278,7 @@ bot.command('getnames', async (ctx) => {
 
 bot.hears('ping', (ctx) => ctx.reply('pong'));
 
-schedule.scheduleJob('0 0 */1 * * *', async function(){
+schedule.scheduleJob('0 0 */1 * * *', async function() {
   let currencies;
   let subscribed;
   try {
@@ -317,6 +318,14 @@ schedule.scheduleJob('0 0 */1 * * *', async function(){
   }
 
   logger.info(`[SCHEDULER] ping`)
+});
+
+schedule.scheduleJob('0 */5 * * * *', async function() {
+  request('https://tg-pinger.herokuapp.com/', function (error, response, body) {
+      console.error('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      console.log('body:', body); // Print the HTML for the Google homepage.
+  });
 });
 
 bot.launch();
